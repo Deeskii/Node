4882a39a6b694d2a0dab4ef47390d45e12cc88e1
Dee Ragoo <deeskii32443@gmail.com>
2021-04-19 18:19:58 -0400

    model with pg

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { indexPage } from '../controllers';
+import { messagesPage } from '../controllers/messages';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
-
+indexRouter.get('/messages', messagesPage);
 export default indexRouter;
