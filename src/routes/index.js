ddf31cc5d0951dbe228c693c049f0dbe0887b419
Dee Ragoo <deeskii32443@gmail.com>
2021-04-20 01:22:07 -0400

    finishing db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { indexPage } from '../controllers';
+// eslint-disable-next-line import/named
+import { indexPage, addMessage } from '../controllers';
 import { messagesPage } from '../controllers/messages';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', addMessage);
 export default indexRouter;
