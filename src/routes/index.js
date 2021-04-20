bdc8ffd72c2a1d4860dab2eacdb0924316f6c650
Dee Ragoo <deeskii32443@gmail.com>
2021-04-20 01:42:31 -0400

    axios

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 // eslint-disable-next-line import/named
 import { indexPage } from '../controllers';
 import { messagesPage, addMessage } from '../controllers/messages';
-import { modifyMessage } from '../middleware';
+import { modifyMessage, performAsyncAction  } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
 indexRouter.post('/messages', addMessage);
-indexRouter.post('/messages', modifyMessage, addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage, performAsyncAction );
 export default indexRouter;
