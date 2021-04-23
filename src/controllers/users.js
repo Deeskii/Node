ef6ce7aa01a86b321962d841f92aa3093d4bca27
Deeskii <deeskii32443@gmail.com>
2021-04-23 05:27:39 -0400

    users controller

---
+++ src/controllers/users.js
@@ -0,0 +1,24 @@
+import Model from '../models/model';
+
+const usersModel = new Model('users');
+
+export const usersPage = async (req, res) => {
+  try {
+    const data = await usersMdel.select('username');
+    res.status(200).json({ users: data.rows });
+  } catch (err) {
+    res.status(200).json({ users: err.stack });
+  }
+};
+
+export const addUsers = async (req, res) => {
+  const { username, email,  } = req.body;
+  const columns = 'username, email';
+  const values = `'${username}', '${email}'`;
+  try {
+    const data = await usersModel.insertWithReturn(columns, values);
+    res.status(200).json({ users: data.rows });
+  } catch (err) {
+    res.status(200).json({ users: err.stack });
+  }
+};
